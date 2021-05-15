2a9a18a724affd88df6591ddfb381592a8fc370c
Mau Reyes <reyes.marq@gmail.com>
2021-05-14 18:55:28 -0600

    Initial commit

---
+++ greet/greet.go
@@ -0,0 +1,11 @@
+package greet
+
+var name = "Mau"
+
+func English() string {
+	return "Hello " + name
+}
+
+func Italian() string {
+	return "Ciao " + name
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package paquetesmodulos
+
+import "fmt"
+
+func main() {
+	fmt.Println("Main function")
+}
